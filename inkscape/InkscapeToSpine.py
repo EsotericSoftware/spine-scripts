5a220269f6c60339914cbc2f8be075d37fe54cc0
Nathan Sweet <nathan.sweet@gmail.com>
2017-08-25 22:56:25 +0200

    Organized license files.

--- inkscape/InkscapeToSpine.py
+++ inkscape/InkscapeToSpine.py
@@ -1,35 +1,13 @@
 #!/usr/bin/env python
 """
-Copyright (c) Jerome Leclanche
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-Original hosting location:
-https://github.com/jleclanche/inkscape-spine-exporter
-"""
-
-"""
 Spine export for Inkscape
 
 Export each layer in the current document to individual PNG files and generate
 a Spine JSON file to import.
 https://esotericsoftware.com/spine-json-format
+
+Original hosting location:
+https://github.com/jleclanche/inkscape-spine-exporter
 """
 
 import os
