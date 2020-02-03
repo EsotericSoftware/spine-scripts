57c17e1c51bb029b056e95b0f34d1265d2c13aba
NathanSweet <nathan.sweet@gmail.com>
2020-02-03 03:03:23 +0100

    Scale styles when scaling image.
    closes EsotericSoftware/spine-editor#506

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -13,7 +13,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 4.5; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 4.6; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var cs2 = parseInt(app.version) < 10;
 
@@ -910,8 +910,12 @@ function stripName (name) {
 }
 
 function scaleImage () {
-	var imageSize = activeDocument.width.as("px") * settings.scale;
-	activeDocument.resizeImage(UnitValue(imageSize, "px"), null, null, ResampleMethod.BICUBICSHARPER);
+	var desc = new ActionDescriptor();
+	desc.putUnitDouble(cID("Wdth"), cID("#Prc"), settings.scale * 100);
+	desc.putBoolean(sID("scaleStyles"), true);
+	desc.putBoolean(cID("CnsP"), true);
+	desc.putEnumerated(cID("Intr"), cID("Intp"), sID("bicubicSharper"));
+	executeAction(cID("ImgS"), desc, DialogModes.NO);
 }
 
 var history;
