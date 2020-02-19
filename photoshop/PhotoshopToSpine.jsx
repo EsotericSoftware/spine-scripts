6837368ea947cab3c8edd3e5d07c867decbf6b49
NathanSweet <nathan.sweet@gmail.com>
2020-02-19 14:12:23 +0100

    Fixed CS2 choking on rasterizeStyles.

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -5,7 +5,7 @@ app.bringToFront();
 // writes a JSON file which can be imported into Spine where the images
 // will be displayed in the same positions and draw order.
 
-// Copyright (c) 2012-2017, Esoteric Software
+// Copyright (c) 2012-2020, Esoteric Software
 // All rights reserved.
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 //     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
@@ -13,7 +13,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 4.8; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 4.9; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var cs2 = parseInt(app.version) < 10;
 
@@ -914,13 +914,15 @@ function rasterizeAll () {
 }
 
 function rasterizeStyles (layer) {
-	activeDocument.activeLayer = layer;
-	var desc = new ActionDescriptor();
-	var ref = new ActionReference();
-	ref.putEnumerated(cID("Lyr "), cID("Ordn"), cID("Trgt"));
-	desc.putReference(cID("null"), ref);
-	desc.putEnumerated(cID("What"), sID("rasterizeItem"), sID("layerStyle"));
-	executeAction(sID("rasterizeLayer"), desc, DialogModes.NO);
+	try {
+		activeDocument.activeLayer = layer;
+		var desc = new ActionDescriptor();
+		var ref = new ActionReference();
+		ref.putEnumerated(cID("Lyr "), cID("Ordn"), cID("Trgt"));
+		desc.putReference(cID("null"), ref);
+		desc.putEnumerated(cID("What"), sID("rasterizeItem"), sID("layerStyle"));
+		executeAction(sID("rasterizeLayer"), desc, DialogModes.NO);
+	} catch (ignored) {}
 }
 
 function scaleImage () {
