ae16236f36a135cd1e99a1751cec3a7c1618b7cb
Nathan Sweet <nathan.sweet@gmail.com>
2020-12-17 22:26:07 -0800

    Allow merge tag on a layer, the user may have merged manually to save time.
    http://esotericsoftware.com/forum/suggestion-PS-export-script-merge-single-layer-issue-15141

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -13,7 +13,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 5.3; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 5.4; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var cs2 = parseInt(app.version) < 10;
 
@@ -760,7 +760,7 @@ function collectLayers (parent, collect) {
 					error(message);
 					continue outer;
 				}
-			} else if (!isValidLayerTag(tag)) {
+			} else if (tag != "merge" && !isValidLayerTag(tag)) { // Allow merge, the user may have merged manually to save time.
 				var message = "Invalid layer name:\n\n" + layer.name;
 				if (isValidGroupTag(tag))
 					message += "\n\nThe [" + tag + "] tag is only valid for groups, not for layers.";
