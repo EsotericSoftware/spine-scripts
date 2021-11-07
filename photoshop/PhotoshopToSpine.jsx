905b5e4ce1e66fa6633e980d219c35183279b32f
Nathan Sweet <nathan.sweet@gmail.com>
2021-11-07 10:27:28 -0400

    Fixed error instead of message when no Photoshop document is open.

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -13,7 +13,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 7.09; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 7.10; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var cs2 = parseInt(app.version) < 10, cID = charIDToTypeID, sID = stringIDToTypeID;
 
@@ -21,7 +21,6 @@ var originalDoc, settings, progress, cancel, errors;
 try {
 	originalDoc = activeDocument;
 } catch (ignored) {}
-var docWidth = activeDocument.width.as("px"), docHeight = activeDocument.height.as("px");
 
 var defaultSettings = {
 	ignoreHiddenLayers: false,
@@ -48,13 +47,14 @@ function run () {
 	var imagesFolder = new Folder(imagesDir);
 	imagesFolder.create();
 
+	var docWidth = originalDoc.width.as("px"), docHeight = originalDoc.height.as("px");
 	var xOffSet = rulerOrigin("H"), yOffSet = rulerOrigin("V");
 
 	try {
 		deleteDocumentAncestorsMetadata();
 	} catch (ignored) {}
 
-	activeDocument.duplicate();
+	originalDoc.duplicate();
 	deselectLayers();
 
 	// Uncomment this line to enlarge the canvas so layers are not cropped.
