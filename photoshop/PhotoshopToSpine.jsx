d3ebf8695c04ab78a9b9945ed7b1db46ef96e08a
Nathan Sweet <nathan.sweet@gmail.com>
2021-12-28 00:00:45 -0400

    [7.16] Fixed incorrect error about "Expected attachment name".
    EsotericSoftware/spine-editor#619

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -13,7 +13,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 7.15; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 7.16; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var cs2 = parseInt(app.version) < 10, cID = charIDToTypeID, sID = stringIDToTypeID;
 
@@ -166,7 +166,7 @@ function run () {
 			bone.y -= (docHeight - yOffSet) * settings.scale;
 		}
 
-		var skinName;
+		var skinName = null;
 		var skinLayer = layer.findTagLayer("skin");
 		if (skinLayer) {
 			skinName = skinLayer.getTagValue("skin");
@@ -282,7 +282,7 @@ function run () {
 	var n = errors.length;
 	if (n) {
 		var first = errors[0];
-		var file;
+		var file = null;
 		if (n > 1) {
 			try {
 				var all = "";
@@ -1184,7 +1184,7 @@ function getLayerID (index) {
 
 function hasBackgroundLayer () {
 	try {
-		var ref = new ActionReference ()
+		var ref = new ActionReference();
 		ref.putEnumerated(sID("document"), sID("ordinal"), sID("targetEnum"));
 		return executeActionGet(ref).getBoolean(sID("hasBackgroundLayer"));
 	} catch (e) { // CS2.
