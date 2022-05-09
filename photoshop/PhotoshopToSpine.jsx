d97a0b866c1fc43ea3eaf7b5024a43814d0f7130
Nathan Sweet <nathan.sweet@gmail.com>
2022-05-09 13:12:43 -0400

    [7.21] Fixed hasBackgroundLayer crash with some Photoshop installations.
    http://esotericsoftware.com/forum/Photoshop-Empty-error-dialog-box-before-crash-17466?p=76673#p76673

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -13,7 +13,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 7.20; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 7.21; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var cs2 = parseInt(app.version) < 10, cID = charIDToTypeID, sID = stringIDToTypeID;
 
@@ -1193,17 +1193,18 @@ function getLayerID (index) {
 }
 
 function hasBackgroundLayer () {
-	try {
-		var ref = new ActionReference();
-		ref.putEnumerated(sID("document"), sID("ordinal"), sID("targetEnum"));
-		return executeActionGet(ref).getBoolean(sID("hasBackgroundLayer"));
-	} catch (e) { // CS2.
-		try {
-			return activeDocument.backgroundLayer;
-		} catch (ignored) {
-		}
-		return false;
-	}
+   try {
+      var ref = new ActionReference(); 
+      ref.putProperty(cID("Prpr"), sID("hasBackgroundLayer")); 
+      ref.putEnumerated(cID("Dcmn"), cID("Ordn"), cID("Trgt"));
+      return executeActionGet(ref).getBoolean(sID("hasBackgroundLayer"));
+   } catch (e) { // CS2.
+      try {
+         return activeDocument.backgroundLayer;
+      } catch (ignored) {
+      }
+      return false;
+   }
 }
 
 function typeToMethod (type) {
