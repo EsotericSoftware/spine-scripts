1faf34763a23ea673d4ca68703631a28c9538b42
Nathan Sweet <nathan.sweet@gmail.com>
2022-09-25 08:28:40 -0400

    [7.29] Fixed meshes being flipped.

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -14,7 +14,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = 7.28; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = 7.29; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var revealAll = false; // Set to true to enlarge the canvas so layers are not cropped.
 var legacyJson = false; // Set to true to output the old Spine JSON format.
@@ -472,10 +472,10 @@ function run () {
 						if (mesh.skinName) json += '"skin": "' + mesh.skinName + '", ';
 					}
 					json += '"width": ' + width + ', "height": ' + height + ', "vertices": [ ';
-					json += x + ', ' + (y - height) + ', ';
 					json += (x + width) + ', ' + (y - height) + ', ';
-					json += (x + width) + ', ' + y + ', ';
-					json += x + ', ' + y + ' ], "uvs": [ 1, 1, 0, 1, 0, 0, 1, 0 ], "triangles": [ 1, 2, 3, 1, 3, 0 ], "hull": 4, "edges": [ 0, 2, 2, 4, 4, 6, 0, 6 ]';
+					json += x + ', ' + (y - height) + ', ';
+					json += x + ', ' + y + ', ';
+					json += (x + width) + ', ' + y + ' ], "uvs": [ 1, 1, 0, 1, 0, 0, 1, 0 ], "triangles": [ 1, 2, 3, 1, 3, 0 ], "hull": 4, "edges": [ 0, 2, 2, 4, 4, 6, 0, 6 ]';
 				} else {
 					json += '"x": ' + x + ', "y": ' + y + ', "width": ' + width + ', "height": ' + height;
 					if (scale != 1) json += ', "scaleX": ' + (1 / scale) + ', "scaleY": ' + (1 / scale);
