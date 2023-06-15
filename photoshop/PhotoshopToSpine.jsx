1594e26de950a17959fe9dd3764b0d10d52aa1a7
Nathan Sweet <nathan.sweet@gmail.com>
2023-06-14 21:14:56 -0400

    Fixed settings not persisting after Photoshop is closed.
    closes EsotericSoftware/spine-editor#748

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -14,7 +14,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = "7.31"; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = "7.32"; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var revealAll = false; // Set to true to enlarge the canvas so layers are not cropped.
 var legacyJson = true; // Set to false to output the newer Spine JSON format.
@@ -820,7 +820,7 @@ function showSettingsDialog () {
 function loadSettings () {
 	var options;
 	try {
-		options = app.getCustomOptions(sID("settings"));
+		options = app.getCustomOptions("PhotoshopToSpine");
 	} catch (ignored) {}
 
 	settings = {};
@@ -841,7 +841,7 @@ function saveSettings () {
 		if (!defaultSettings.hasOwnProperty(key)) continue;
 		desc["put" + getOptionType(defaultSettings[key])](sID(key), settings[key]);
 	}
-	app.putCustomOptions(sID("settings"), desc, true);
+	app.putCustomOptions("PhotoshopToSpine", desc, true);
 }
 
 function getOptionType (value) {
