0e4373f74adcf9ab5f3141cb143e9c77053c0c05
Davide Tantillo <7047501+davidetan@users.noreply.github.com>
2026-07-13 11:40:17 +0200

    [7.41] Fixed linked mesh using source in json field in 4.3

--- photoshop/PhotoshopToSpine.jsx
+++ photoshop/PhotoshopToSpine.jsx
@@ -14,7 +14,7 @@ app.bringToFront();
 //     * Neither the name of Esoteric Software nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var scriptVersion = "7.40"; // This is incremented every time the script is modified, so you know if you have the latest.
+var scriptVersion = "7.41"; // This is incremented every time the script is modified, so you know if you have the latest.
 
 var revealAll = false; // Set to true to enlarge the canvas so layers are not cropped.
 var legacyJson = true; // Set to false to output the newer Spine JSON format.
@@ -470,7 +470,7 @@ function run () {
 					if (mesh === true)
 						json += '"type": "mesh", ';
 					else {
-						json += '"type": "linkedmesh", "parent": "' + mesh.placeholderName + '", ';
+						json += '"type": "linkedmesh", "source": "' + mesh.placeholderName + '", "parent": "' + mesh.placeholderName + '", ';
 						if (mesh.skinName) json += '"skin": "' + mesh.skinName + '", ';
 					}
 					json += '"width": ' + width + ', "height": ' + height + ', "vertices": [ ';
@@ -1131,7 +1131,7 @@ function jsonPath (jsonPath) {
 		var index = forwardSlashes(jsonPath).lastIndexOf("/");
 		if (index != -1) return absolutePath(jsonPath.slice(0, index + 1)) + jsonPath.slice(index + 1);
 		return absolutePath("./") + jsonPath;
-	} 
+	}
 	var name = decodeURI(originalDoc.name);
 	return absolutePath(jsonPath) + name.substring(0, name.indexOf(".")) + ".json";
 }
@@ -1172,7 +1172,7 @@ function rulerOrigin (axis) {
 	var key = cID("Rlr" + axis);
 	var ref = new ActionReference();
 	ref.putProperty(cID("Prpr"), key);
-	ref.putEnumerated(cID("Dcmn"), cID("Ordn"), cID("Trgt")); 
+	ref.putEnumerated(cID("Dcmn"), cID("Ordn"), cID("Trgt"));
 	return executeActionGet(ref).getInteger(key) >> 16;
 }
 
@@ -1312,8 +1312,8 @@ function getLayerID (index) {
 
 function hasBackgroundLayer () {
    try {
-      var ref = new ActionReference(); 
-      ref.putProperty(cID("Prpr"), sID("hasBackgroundLayer")); 
+      var ref = new ActionReference();
+      ref.putProperty(cID("Prpr"), sID("hasBackgroundLayer"));
       ref.putEnumerated(cID("Dcmn"), cID("Ordn"), cID("Trgt"));
       return executeActionGet(ref).getBoolean(sID("hasBackgroundLayer"));
    } catch (e) { // CS2.
